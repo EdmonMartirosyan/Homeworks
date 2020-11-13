39fd259e202b182387c435c9c2e4b58d13f1d0e8
EdmonMartirosyan <72992314+EdmonMartirosyan@users.noreply.github.com>
2020-11-13 12:58:34 +0400

    Update Homework(N).py

--- Homework(N).py
+++ Homework(N).py
@@ -14,3 +14,19 @@
 #
 #
 # find_counterfeit_coin([2, 2, 2, 2, 2, 2, 2, 1])
+
+
+# 2
+# def sort_list_elements(numbers):
+#     sorted_list = []
+#     for i in range(0, len(numbers)):
+#         if numbers[i] != -1:
+#             sorted_list.append(numbers[i])
+#     sorted_list.sort()
+#     for i in range(0, len(numbers)):
+#         if numbers[i] == -1:
+#             sorted_list.insert(i, numbers[i])
+#     return sorted_list
+
+
+#print(sort_list_elements([2, -1, 1, 5, 4, -1, 3]))
